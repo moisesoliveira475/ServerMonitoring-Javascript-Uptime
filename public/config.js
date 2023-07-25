a7200c56789e6668b21a073e4ef2e80dfcaee15f
Moises Santos De Oliveira <102919795+moisesoliveira475@users.noreply.github.com>
2023-07-25 13:57:40 +0000

    configure dependencies

---
+++ public/config.js
@@ -0,0 +1,32 @@
+window.Config = {
+    //show title
+    siteName: `public status`,
+
+    // API Keys of UptimeRobot
+    // Monitor-Specific and Read-Only Support
+ApiKeys: [
+    '',
+    '',
+    '',
+    '',
+],
+    // Register Days
+    CountDays: 90,
+    // Mostrar link do site de verificação
+    showLink: true,
+    //Navybar
+    Navi: [
+        {
+            text: 'Homepage',
+            url: ''
+        },
+        {
+            text: `GitHub`,
+            url: `https://github.com/moisesoliveira475`
+        },
+        {
+            text: `Blog`,
+            url: ``
+        }
+    ],
+};
