e0b567fcee363712c4301541d0ea1f822d055385
Moises Santos De Oliveira <102919795+moisesoliveira475@users.noreply.github.com>
2023-07-26 17:44:37 +0000

    helper

---
+++ src/common/helper.js
@@ -0,0 +1,20 @@
+export function formatNumber(value) {
+    return(Math.floor(value * 100) / 100).toString();
+}
+export function formatDuration(seconds) {
+    let s = parseInt(seconds);
+    let m = 0;
+    let h = 0;
+    if (s >= 60) {
+        m = parseInt(s / 60);
+        s = parseInt(s % 60);
+        if(m >= 60) {
+            h = parseInt(m / 60);
+            m = parseInt(m % 60);
+        }
+    }
+    let text = `${s} seconds`;
+    if(m > 0) text = `${m} minutes ${text}`
+    if(h > 0) text = `${h} hours ${text}`
+    return text;
+}
